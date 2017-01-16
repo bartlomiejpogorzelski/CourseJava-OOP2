db122391b0c17f86ce0320cc0b50b8160c46321b
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2017-01-16 17:14:08 +0100

    Instance of keywords + excercise abstraction
    initiation

--- src/coursejava/oop2/CourseJavaOOP2.java
+++ src/coursejava/oop2/CourseJavaOOP2.java
@@ -1,36 +1,11 @@
 
 package coursejava.oop2;
-    
-//Packages
-import coursejava.oop2.monsters.Monster;
-import coursejava.oop2.monsters.Sceleton;
-import coursejava.oop2.monsters.Zombie;
 
 
 public class CourseJavaOOP2 {
 
     public static void main(String[] args) {
-    // MONSTERS - ZOMBIES - SKELETONS - hit points, attack, speed
-//       Monster monster = new Monster(100,10);
-//       
-//       monster.Attack();
-//       
-//        System.out.println(monster.hitPoints);
-//        System.out.println(monster.speed);
-//        
-        
-        Monster m = new Sceleton();
-        ((Sceleton)m).test2(); // it's downcasting / now we have access to test2()
-        
-//        m.attack();
-//        m.test();
-        //m.test2(); //This is don't work
-        
-        // when "m." we have access only to the class Monster 
-    }
+   
     
-    static void nazwa(Monster m3)
-    {
-        
     }
 }
