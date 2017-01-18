e8f87d2d77bd0e3c68c35f470872d54837e654a7
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2017-01-18 12:10:28 +0100

    Instanceof keywors + excercise abstraction

--- src/coursejava/oop2/CourseJavaOOP2.java
+++ src/coursejava/oop2/CourseJavaOOP2.java
@@ -6,6 +6,54 @@ public class CourseJavaOOP2 {
 
     public static void main(String[] args) {
    
-    
+    //Person person = new Person(); // this don't work
+     Person person = new Employee("s", "a", 1);
     }
 }
+    abstract class Person 
+    {
+        String name;
+        String surname;
+        
+        Person()
+        {
+            
+        }
+        Person (String name, String surname)
+        {
+            this.name=name;
+            this.surname=surname;
+        }
+        
+        abstract void description();
+    }
+    
+    class Employee extends Person
+    {
+        double salary;
+        public Employee(String name, String surname, double salary)
+        {
+            super(name, surname);
+            this.salary=salary;
+        }
+        @Override
+        void description() {
+            System.out.println("name = " + name);
+            System.out.println("surname = " + surname);
+        }
+        
+    }
+    
+    class Student extends Person
+    {
+        public Student(String name, String surname)
+        {
+        super(name, surname);
+        }
+        @Override
+        void description() {
+            
+        }
+        
+    }
+
