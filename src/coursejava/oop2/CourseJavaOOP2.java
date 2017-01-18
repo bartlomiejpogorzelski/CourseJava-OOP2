ad6f0ec3201924728dfc078af5bc49ba41c4fdf4
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2017-01-18 13:01:25 +0100

    Instanceof keywords + excercise of abstraction

--- src/coursejava/oop2/CourseJavaOOP2.java
+++ src/coursejava/oop2/CourseJavaOOP2.java
@@ -11,6 +11,8 @@ public class CourseJavaOOP2 {
       Person people[] = new Employee[2];
       people[0]= new Employee("Arkadiusz", "wlodarczyk", 1000);
       people[1]= new Employee("ola", "wlodarczyk", 1100);
+      
+      people[0].description();
     }
 }
     abstract class Person 
