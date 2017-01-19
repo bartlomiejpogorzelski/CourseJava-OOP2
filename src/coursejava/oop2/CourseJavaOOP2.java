c9f96aab1d0e1b3698c9c34a653bafa7664ad3a1
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2017-01-19 15:26:06 +0100

    Final classes...

--- src/coursejava/oop2/CourseJavaOOP2.java
+++ src/coursejava/oop2/CourseJavaOOP2.java
@@ -1,13 +1,15 @@
 
 package coursejava.oop2;
 
+import java.util.Date;
+
 
 public class CourseJavaOOP2 {
 
     public static void main(String[] args) {
    
     final double PI= 3.14;
-    PI= 5.55; //we cannot change the value beacuse is use final
+    //PI= 5.55; //we cannot change the value beacuse is use final
     }
 }
     abstract class Person 
@@ -17,7 +19,11 @@ public class CourseJavaOOP2 {
     
     class Employee extends Person
     {
-       
+        //final Date dateOfEmployment //only this is bad beacuse with "final" it must be initialize
+        final Date dateOfEmployment = new Date();
+        
+        
+        
     }
     
    
