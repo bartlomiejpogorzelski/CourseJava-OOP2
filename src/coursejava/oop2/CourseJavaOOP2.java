f6bb7195d43c8a25f1bc0134e6c736220613735c
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2017-01-19 22:37:47 +0100

    Interfaces

--- src/coursejava/oop2/CourseJavaOOP2.java
+++ src/coursejava/oop2/CourseJavaOOP2.java
@@ -10,4 +10,11 @@ public class CourseJavaOOP2 {
     }
 }
     
-   
+interface nameOfInterfaces
+{
+    double PI =3.14;  // public final static
+    
+    void sample(); // method is public and abstract 
+}   
+
+//implements means you want to take after all behaviors from the inteface 
