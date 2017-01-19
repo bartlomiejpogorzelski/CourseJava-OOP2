9a981f6735d8fc93858245f64f86bfb0c57b2898
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2017-01-19 15:13:11 +0100

    Final classes...
    final

--- src/coursejava/oop2/CourseJavaOOP2.java
+++ src/coursejava/oop2/CourseJavaOOP2.java
@@ -6,7 +6,8 @@ public class CourseJavaOOP2 {
 
     public static void main(String[] args) {
    
-   
+    final double PI= 3.14;
+    PI= 5.55; //we cannot change the value beacuse is use final
     }
 }
     abstract class Person 
